a5a4e9b0fac2a2047734b8e06723dbcb95138835
Santanu Lahiri <slahiri@krypton.local>
2018-02-12 14:49:30 -0500

    Escaping connection string for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const REMOTE_MONGO = 'mongodb://slahiri201:B1gF@tT0d0@ds233208.mlab.com:33208/node-test-todo';
+const REMOTE_MONGO = 'mongodb://slahiri201:B1gF\@tT0d0\@ds233208.mlab.com:33208/node-test-todo';
 const LOCAL_MONGO = 'mongodb://localhost:27017/ToDoApp';
 
 const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
