ab85da2911c76ed94d36a965f98460eff8a7a104
Santanu Lahiri <slahiri@krypton.local>
2018-02-11 23:48:26 -0500

    heroku deployments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,16 @@
 const mongoose = require ('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect ('mongodb://localhost:27017/ToDoApp');
+
+const REMOTE_MONGO = 'mongodb://slahiri201:B1gF@tT0d0@ds233208.mlab.com:33208/node-test-todo';
+const LOCAL_MONGO = 'mongodb://localhost:27017/TodoApp';
+
+const MONGO_URI = process.env.PORT ? REMOTE_MONGO : LOCAL_MONGO;
+
+mongoose.connect (MONGO_URI).then(() => {
+  console.log('Connected to Mongo instance.')
+}, (err) => {
+  console.log('Error connecting to Mongo instance: ', err);
+});
 
 module.exports = { mongoose };
